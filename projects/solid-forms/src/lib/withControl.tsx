4f591108e03061e0c18c9997f39018652e9ca325
John <12599934+jorroll@users.noreply.github.com>
2022-06-06 09:11:42 +0000

    feat: add controlClassList prop

--- projects/solid-forms/src/lib/withControl.tsx
+++ projects/solid-forms/src/lib/withControl.tsx
@@ -1,5 +1,13 @@
-import { Component, createMemo, createRoot, JSX, ParentProps } from 'solid-js';
-import { IAbstractControl } from './abstract-control';
+import {
+  Accessor,
+  Component,
+  createMemo,
+  createRoot,
+  JSX,
+  ParentProps,
+} from "solid-js";
+import { IAbstractControl } from "./abstract-control";
+import { isAbstractControlContainer } from "./abstract-control-container";
 
 type WithControlProps<
   Props,
@@ -21,6 +29,7 @@ export interface IWithControlOptions<
   component: Component<
     WithControlProps<Props, ControlFactory> & {
       control: ReturnType<ControlFactory>;
+      controlClassList: Accessor<ReturnType<typeof createClassList>>;
     }
   >;
 }
@@ -73,12 +82,42 @@ export function withControl<
         )
     );
 
+    const controlClassList = () => createClassList(control());
+
     const Component = options.component;
 
-    return <Component {...props} control={control()} />;
+    return (
+      <Component
+        {...props}
+        control={control()}
+        controlClassList={controlClassList}
+      />
+    );
   };
 
   wrappedComponent.control = options.controlFactory;
 
   return wrappedComponent;
+}
+
+export function createClassList(control: IAbstractControl) {
+  return {
+    "sf-control-container": isAbstractControlContainer(control),
+    "sf-valid": control.isValid,
+    "sf-invalid": !control.isValid,
+    "sf-dirty": control.isDirty,
+    "sf-not-dirty": !control.isDirty,
+    "sf-touched": control.isTouched,
+    "sf-untouched": !control.isTouched,
+    "sf-pending": control.isPending,
+    "sf-not-pending": !control.isPending,
+    "sf-disabled": control.isDisabled,
+    "sf-enabled": !control.isDisabled,
+    "sf-editable": !control.isReadonly,
+    "sf-readonly": control.isReadonly,
+    "sf-optional": !control.isRequired,
+    "sf-required": control.isRequired,
+    "sf-submitted": control.isSubmitted,
+    "sf-not-submitted": !control.isSubmitted,
+  };
 }
